dcfffe8c33037bcddb0fe72780805705f9cd898f
Erick Kurniawan <erick.kurniawan@gmail.com>
2024-03-06 13:49:50 +0700

    static categories

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -5,15 +5,14 @@ namespace SampleMVC.Controllers;
 
 public class CategoriesController : Controller
 {
-    private List<Category> categories = new List<Category>
-    {
+    private static List<Category> categories = new List<Category>() {
         new Category { CategoryID = 1, CategoryName = "Beverages" },
         new Category { CategoryID = 2, CategoryName = "Condiments" },
         new Category { CategoryID = 3, CategoryName = "Confections" },
         new Category { CategoryID = 4, CategoryName = "Dairy Products" },
         new Category { CategoryID = 5, CategoryName = "Grains/Cereals" },
         new Category { CategoryID = 6, CategoryName = "Meat/Poultry" },
-        new Category { CategoryID = 7, CategoryName = "Produce" },
+        new Category { CategoryID = 7, CategoryName = "Produce" }
     };
 
     public IActionResult Index()
@@ -32,5 +31,17 @@ public class CategoriesController : Controller
                         select c).SingleOrDefault();*/
         var category = categories.SingleOrDefault(c => c.CategoryID == id);
         return View(category);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Category category)
+    {
+        categories.Add(category);
+        return RedirectToAction("Index");
     }
 }
